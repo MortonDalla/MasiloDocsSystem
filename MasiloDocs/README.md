0d7c3467fc80dd37072f9e0ebf53f50f8d86e5e8
morton <mortondala1@gmail.com>
2019-11-18 07:25:18 +0200

    reinitialized files

---
+++ MasiloDocs/README.md
@@ -0,0 +1,6 @@
+# MasiloDocs
+
+![HealthCareSys](healthcare/static/img/logo.png)
+
+HealthCareSys is a django based Online Health Care System
+
---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\duncan tlemo\videos\desktop\kat\healthcaresys-master\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
